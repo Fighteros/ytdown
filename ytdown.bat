3b0834f58acf94aa1605344162d6d333cd4946fd
Ahmed M. Abd Elghany <am5419@fayoum.edu.eg>
2025-02-13 17:01:23 +0200

    latest updates

--- ytdown.bat
+++ ytdown.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Users\am541\AppData\Local\Programs\Python\Python310\python.exe" "D:\projects\python_projects\get_down_links\ytdown.py"
+C:\Users\am541\AppData\Local\Programs\Python\Python310\python.exe "D:\projects\Python\ytdown\ytdown.py"
